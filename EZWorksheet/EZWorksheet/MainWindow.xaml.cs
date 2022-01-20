8909ae8ffe8f87738091e01a885e3aaf69dae234
ShaharMS <shaharmsecond@gmail.com>
2022-01-21 01:05:29 +0200

    added the WPF EZWorksheet project

---
+++ EZWorksheet/EZWorksheet/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EZWorksheet
+{
+    //VARIABLE | Title           | TYPE IS | TextBlock
+    //VARIABLE | CreateWorksheet | TYPE IS | Button
+    //VARIABLE | ScanWorksheet   | TYPE IS | Button
+
+
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            CreateWorksheet.Content = (int.Parse(CreateWorksheet.Content.ToString()) + 1).ToString();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            ScanWorksheet.Content = (int.Parse(ScanWorksheet.Content.ToString()) + 1).ToString();
+        }
+    }
+}
