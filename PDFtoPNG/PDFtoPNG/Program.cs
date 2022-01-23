ec1f6102509d78da7ed8bcc23b4b4299612f2550
ShaharMS <shaharmsecond@gmail.com>
2022-01-23 12:43:35 +0200

    finished PDF convertion

--- PDFtoPNG/PDFtoPNG/Program.cs
+++ PDFtoPNG/PDFtoPNG/Program.cs
@@ -26,36 +26,65 @@ namespace PDFtoPNG
             //checks if the file Path.txt is present, else create it
             File.AppendText("Path.txt").Close();
 
+            //checks if the file RunDetails is present, else create it
+            var stream = File.AppendText("ConvertionFinished.txt");
+            //if the program finshied the convertion. will always be on the first line
+            stream.Write("false");
+            stream.Close();
+            //the amount of PDF pages, the same as the amount of images being converted
+            stream = File.AppendText("PageCount.txt");
+            stream.Write("0");
+            stream.Close();
+            //exit code:
+            // 0 - success
+            // 1 - fail
+            // 2 - never started
+            //whenever you convert a file again the value will be set to 2, and then will be set to either 1\0 depending on the outcome
+            stream = File.AppendText("ExitCode.txt");
+            stream.WriteLine("2");
+            stream.Close();
             //actually read the content
             var pdfpath = File.ReadAllText("Path.txt");
+            var savePath = Environment.CurrentDirectory;
 
             // open and load the file
             if (pdfpath == "") return;
-            using (FileStream fs = new FileStream(pdfpath, FileMode.Open))
+            try
             {
-                var document = new Document(fs);
-                // process and save pages one by one
-                
-
-                for (int i = 0; i < document.Pages.Count; i++)
+                using (FileStream fs = new FileStream(pdfpath, FileMode.Open))
                 {
-                    
-                    Page currentPage = document.Pages[i];
-                    byte[] ByteArray = currentPage.RenderAsBytes((int)currentPage.Width, (int)currentPage.Height, new RenderingSettings());
-                    using (Bitmap output = new Bitmap((int)currentPage.Width, (int)currentPage.Height))
+                    var document = new Document(fs);
+                    // process and save pages one by one
+
+
+                    for (int i = 0; i < document.Pages.Count; i++)
                     {
-                        System.Drawing.Rectangle rect = new System.Drawing.Rectangle(0, 0, output.Width, output.Height);
-                        BitmapData bmpData = output.LockBits(rect, ImageLockMode.ReadWrite, output.PixelFormat);
-                        IntPtr ptr = bmpData.Scan0;
-                        Marshal.Copy(ByteArray, 0, ptr, ByteArray.Length);
-                        output.UnlockBits(bmpData);
-                        File.Create(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + $"\\{i}.png").Close();
-                        output.Save(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + $"\\{i}.png");
+
+                        Page currentPage = document.Pages[i];
+                        byte[] ByteArray = currentPage.RenderAsBytes((int)currentPage.Width, (int)currentPage.Height, new RenderingSettings());
+                        using (Bitmap output = new Bitmap((int)currentPage.Width, (int)currentPage.Height))
+                        {
+                            System.Drawing.Rectangle rect = new System.Drawing.Rectangle(0, 0, output.Width, output.Height);
+                            BitmapData bmpData = output.LockBits(rect, ImageLockMode.ReadWrite, output.PixelFormat);
+                            IntPtr ptr = bmpData.Scan0;
+                            Marshal.Copy(ByteArray, 0, ptr, ByteArray.Length);
+                            output.UnlockBits(bmpData);
+                            File.Create(savePath + $"\\{i + 1}.png").Close();
+                            output.Save(savePath + $"\\{i + 1}.png");
+                        }
+
+
                     }
-                    
-                    
+                    File.WriteAllText("ConvertionFinished.txt", "true");
+                    File.WriteAllText("PageCount.txt", $"{document.Pages.Count}");
+                    File.WriteAllText("ExitCode.txt", "0");
                 }
+            } catch(Exception e)
+            {
+                //keeps last convertion data
+                File.WriteAllText("ExitCode.txt", "1");
             }
+            
         }
     }
 }
