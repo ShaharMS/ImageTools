d25638603c9a47dd6f76403b23457927ded582de
ShaharMS <shaharmsecond@gmail.com>
2022-05-14 23:36:17 +0300

    an end is always a start :)

---
+++ App/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+
+namespace ImageProcessing
+{
+    class Program
+    {
+        public static string ProcessType = "";
+        public static string ImagePath = "";
+
+        static void Main(string[] args)
+        {
+            ProcessType = args[0] != null ? args[0] : throw new Exception("Image Processing Error: Processing Type Not Found/Supplied");
+            ImagePath = args[1] != null ? args[1] : throw new Exception("Image Processing Error: Image Path Not Found/Supplied");        
+            switch (ProcessType)
+            {
+                case "DetectLines":
+                    {
+                        Image < Bgr, Byte> image = new Image<Bgr, Byte>(ImagePath);
+                        var vectors = new VectorOfPointF();
+                        CvInvoke.HoughLines(image, vectors, 0, 0, 40);
+                        Console.WriteLine(vectors.ToString());
+                        return;
+                    }
+                case "DetectText":
+                    {
+                        break;
+                    }
+            }
+        }
+    }
+}
